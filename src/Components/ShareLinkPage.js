e357978fb0edf17e4e2e22ada2fcd7f2f73c6104
Bim <minhle@Bims-MacBook-Pro.local>
2020-05-09 13:10:43 +0700

    finished sharelink front

--- src/Components/ShareLinkPage.js
+++ src/Components/ShareLinkPage.js
@@ -7,14 +7,10 @@ export default class ShareLinkPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state={
+			delEmail: '',
 			currentEmail: '',
 			tripName:this.props.tripName,
-			emails:[
-				'a@g.com',
-				'b@g.com',
-				'c@g.com',
-				'd@g.com'
-			]
+			emails:[]
 		}
 	}
 
@@ -41,14 +37,48 @@ export default class ShareLinkPage extends Component {
 
 	add = () => {
 		let newEmails = this.state.emails;
-		newEmails.push(this.state.currentEmail);
+		let found = newEmails.includes(this.state.currentEmail);
+		if (!found) {
+			newEmails.push(this.state.currentEmail);
+			this.setState({
+				currentEmail: '',
+				emails: newEmails
+			})
+		}
+	}
+
+	del = (email) => {
+		const fEmails = this.state.emails.filter(listemail => {
+			return listemail !== email;
+		})
 		this.setState({
-			currentEmail: '',
-			emails: newEmails
+			delEmail : email,
+			emails: fEmails
 		})
 	}
 
 	render() {
+		const fEmails = this.state.emails.filter(email => {
+			return email !== this.state.delEmail;
+		})
+
+		const rows = [];
+
+		fEmails.forEach(email => {
+			rows.push(
+				<div className='tc flex justify-center'>
+				<p className='pa1 tc white f5 underline'> 
+					{email}
+				</p>
+				<a 
+					className='white f4 pt3 pl2'
+					onClick={() => this.del(email)}>
+				X	
+				</a>
+			</div>
+			)
+		})
+
 		return (
 			<div>
 				<BackBt 
@@ -69,9 +99,13 @@ export default class ShareLinkPage extends Component {
 							onClick={this.add}
 						/>
 					</div> 
-					<div className='pa2 tc'>
-						<MemList
-							list={this.state.emails}
+					<div className='pa2 tc flow'>
+						{rows}
+					</div>
+					<div className='tc '>
+						<Butt
+							value='Share'
+							onClick={this.share}
 						/>
 					</div>
 				</div>
@@ -79,39 +113,3 @@ export default class ShareLinkPage extends Component {
 		)
 	}
 }
-
-class MemList extends Component {
-
-	render() {
-		const list = this.props.list;
-		const rows = [];
-
-		list.forEach(entry => {
-			rows.push(
-				<MemRow 
-					email={entry} 
-				/>
-			)
-		})
-		return (
-			<div>
-				{rows}
-			</div>
-		)
-	}
-} 
-
-class MemRow extends Component {
-
-	render() {
-		const {email} = this.props;
-		return (
-			<p className='pa1 tc white f5 underline'> 
-				{email}
-			</p>
-		)
-	}
-}
-
-
-
